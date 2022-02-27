d1f43c34969de83b8ccda14d7942f01564e85023
Rémi Willebrouck <willremi@gmail.com>
2022-02-27 20:40:01 +0100

    travail sur la page notFound

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer = () => {
+    const year = new Date().getFullYear();
+    
     return (
         <div className='mt-4 p-4 bg-nav-greenOp fixed bottom-0 left-0 w-full'>
-            <div className="text-center text-div-red font-police font-semibold">Rémi W.</div>
+            <div className="text-center text-div-red font-police font-semibold">© {year} - Rémi W.</div>
         </div>
     );
 };
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            page NOTFOUND
+        <div className="flex items-center justify-center h-screen">
+            <div className='bg-div-white rounded-lg border shadow-lg py-2 mx-10 md:px-56 errorCard'>
+                <div className="text-center">
+                <h1 className=' text-8xl'>404</h1>
+                <p>
+                    Houston, on a un problème.
+                    <br />
+                    La page que vous avez demandée semble ne pas exister.
+                </p>
+                </div>
+                <button>Retour à l'accueil</button>
+            </div>
         </div>
     );
 };
