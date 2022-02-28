f64ef88010390376148c52b0b4ad5e37b7944057
Rémi Willebrouck <willremi@gmail.com>
2022-02-28 14:28:16 +0100

    page notfound terminé

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,16 +3,17 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className='bg-div-white rounded-lg border shadow-lg py-2 mx-10 md:px-56 errorCard'>
+            <div className='rounded-lg border shadow-lg py-4 px-4 md:px-56 errorCard'>
                 <div className="text-center">
-                <h1 className=' text-8xl'>404</h1>
-                <p>
-                    Houston, on a un problème.
-                    <br />
+                <h1 className='text-6xl text-red-700'>Erreur 404</h1>
+                <br />
+                <hr className='w-48 mx-auto' />
+                <br />
+                <p className='text-white w-58 mx-auto'>
                     La page que vous avez demandée semble ne pas exister.
                 </p>
+                <button className='border-2 border-div-green hover:border-div-white bg-div-green mt-10 p-1 rounded text-white hover:text-div-red text-center'><a href="/">Retour à l'accueil</a></button>
                 </div>
-                <button>Retour à l'accueil</button>
             </div>
         </div>
     );
