7d78aaf2d42af090f3cfa1d9f29f324077971989
Rémi Willebrouck <willremi@gmail.com>
2022-03-03 14:48:46 +0100

    modification du style responsive de la page notfound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -12,17 +12,19 @@ const NotFound = () => {
 
     return (
         <div className="flex items-center justify-center h-screen">
-            <div className='rounded-lg border shadow-lg py-4 px-4 md:px-56 errorCard'>
+            <div className='rounded-lg border shadow-lg py-4 px-4 sm:px-20 md:px-44 xl:px-56 errorCard'>
                 <div className="text-center">
                 <h1 className='text-6xl text-red-700'>Erreur 404</h1>
                 <br />
                 <hr className='w-48 mx-auto' />
                 <br />
-                <p className='text-white w-58 mx-auto'>
+                <p className='text-white w-58 mx-auto text-xl'>
                     La page que vous avez demandée semble ne pas exister.
                 </p>
-                <button className='border-2 border-div-blue hover:border-div-green bg-div-blue mt-10 p-1 rounded text-white hover:text-div-red text-center'><a href="/">Retour à l'accueil</a></button>
-                <button className='border-2 border-div-blue hover:border-div-green bg-div-blue mt-10 p-1 rounded text-white hover:text-div-red text-center ml-1' onClick={saveFile}>Télécharger le CV</button>
+                <div className="flex justify-around">
+                <button className='border-2 border-div-blue hover:border-div-green bg-div-blue mt-10 p-2 rounded text-white hover:text-div-red text-center'><a href="/" className='text-xl'>Retour à l'accueil</a></button>
+                <button className='border-2 border-div-blue hover:border-div-green bg-div-blue mt-10 p-2 rounded text-white hover:text-div-red text-center ml-1 text-xl' onClick={saveFile}>Télécharger le CV</button>
+                </div>
                 </div>
             </div>
         </div>
