b475120fb1d439761688f9e46658f563c17e13be
Rémi Willebrouck <willremi@gmail.com>
2022-03-07 20:54:49 +0100

    ajout des descriptions dans formationsData

--- src/components/datas/formationsData.js
+++ src/components/datas/formationsData.js
@@ -3,50 +3,126 @@ const Formations = [
         id: 1,
         title: "Incubateur Numérique",
         dates: "13/10/2021 au 10/02/2022",
-        location: "1 FORMATION = 1 JOB - Lille"
+        location: "1 FORMATION = 1 JOB - Lille",
+        actions: [
+            {
+                id: 1,
+                description: "Montée en compétence en utilisant React JS et NodeJs"
+            },
+            {
+                id: 2,
+                description: "Réalisation d'un site web e-commerce"
+            },
+        ]
     },
     {
         id: 2,
         title: "Développeur Web et Web mobile",
         dates: "20/12/2019 au 6/10/2020",
-        location: "AFPA - Liévin"
+        location: "AFPA - Liévin",
+        actions: [
+            {
+                id: 1,
+                description: "Technologies Web : HTML5/CSS3, Javascript, JQuery, AJAX, Bootstrap, SASS"
+            },
+            {
+                id: 2,
+                description: "Langages de programmation : PHP 7, MySQL"
+            },
+            {
+                id: 3,
+                description: "Frameworks : Symfony"
+            },
+            {
+                id: 4,
+                description: "Bases de données : MySQL"
+            },
+        ]
     },
     {
         id: 3,
         title: "Développeur Web",
         dates: "06/2017 au 12/2017",
-        location: "Popschool - Lens"
+        location: "Popschool - Lens",
+        actions: [
+            {
+                id: 1,
+                description: "Technologies Web : HTML5/CSS3, Javascript, Bootstrap"
+            },
+            {
+                id: 2,
+                description: "Langages de programmation : PHP 7, MySQL"
+            },
+            {
+                id: 3,
+                description: "Bases de données : MySQL"
+            },
+        ]
     },
     {
         id: 4,
         title: "Attestation de compétence informatique TIMP@SS",
         dates: "Juin à juillet 2013",
-        location: "Instep - Liévin"
+        location: "Instep - Liévin",
+        actions: [
+            {
+                id: 1,
+                description: "'initiation aux outils bureautiques (traitement de texte, tableur, ...)"
+            }
+        ]
     },
     {
         id: 5,
         title: "Technicien en controle non destructif",
         dates: "4/02/2008 au 29/01/2009",
-        location: "AFPA - Liévin"
+        location: "AFPA - Liévin",
+        actions: [
+            {
+                id: 1,
+                description: "Contrôles surfaciques : ressuage, magnétoscopie"
+            },
+            {
+                id: 2,
+                description: "Contrôles volumiques : radiographies, ultrasons"
+            },
+        ]
     },
     {
         id: 6,
         title: "Niveau BTS génie Optique",
         dates: "09/2003 au 07/2005",
-        location: "Lycée Gustave Eiffel - Armentières"
+        location: "Lycée Gustave Eiffel - Armentières",
+        // actions: [
+        //     {
+        //         id:1,
+        //         description: "Conception de produits faisant intervenir la lumière"
+        //     }
+        // ]
     },
     {
         id: 7,
         title: "1ère année BTS Physiques Industrie et Laboratoire",
         dates: "09/2002 au 07/2003",
-        location: "Lycée Gustave Eiffel - Armentières"
+        location: "Lycée Gustave Eiffel - Armentières",
+        // actions: [
+        //     {
+        //         id:1,
+        //         description: "Conception de produits faisant intervenir la lumière"
+        //     }
+        // ]
     },
     {
         id: 8,
         title: "Bac Sciences de Technologies et Laboratoire-",
         dates: "09/2001 au 07/2002",
-        location: "Lycée Henri Darras - Liévin"
-    }, 
+        location: "Lycée Henri Darras - Liévin",
+        // actions: [
+        //     {
+        //         id:1,
+        //         description: "Conception de produits faisant intervenir la lumière"
+        //     }
+        // ]
+    },
 ]
 
 export default Formations;
--- src/pages/Formations.js
+++ src/pages/Formations.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Formations = () => {
     return (
         <div className='flex items-center justify-center mt-20 mb-20'>
+            <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-3'>
             page Formations
+            
+            </div>
         </div>
     );
 };
