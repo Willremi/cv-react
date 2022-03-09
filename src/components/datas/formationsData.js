3ec0cd5076beee674cebc707205019abbcbd6b2a
Rémi Willebrouck <willremi@gmail.com>
2022-03-09 20:30:25 +0100

    insertion de disclosure dans  la page formations

--- src/components/datas/formationsData.js
+++ src/components/datas/formationsData.js
@@ -1,23 +1,91 @@
 const FormationsInfo = [
     {
         id: 1,
-        title: "Incubateur Numérique",
-        dates: "13/10/2021 au 10/02/2022",
-        location: "1 FORMATION = 1 JOB - Lille",
+        title: "Bac Sciences de Technologies et Laboratoire",
+        dates: "09/2001 au 07/2002",
+        location: "Lycée Henri Darras - Liévin",
+        // actions: [
+        //     {
+        //         id:1,
+        //         description: "Conception de produits faisant intervenir la lumière"
+        //     }
+        // ]
+    },
+    {
+        id: 2,
+        title: "1ère année BTS Physiques Industrie et Laboratoire",
+        dates: "09/2002 au 07/2003",
+        location: "Lycée Gustave Eiffel - Armentières",
+        // actions: [
+        //     {
+        //         id:1,
+        //         description: "Conception de produits faisant intervenir la lumière"
+        //     }
+        // ]
+    },
+    {
+        id: 3,
+        title: "Niveau BTS génie Optique",
+        dates: "09/2003 au 07/2005",
+        location: "Lycée Gustave Eiffel - Armentières",
+        // actions: [
+        //     {
+        //         id:1,
+        //         description: "Conception de produits faisant intervenir la lumière"
+        //     }
+        // ]
+    },
+    {
+        id: 4,
+        title: "Formation Technicien en controle non destructif",
+        dates: "4/02/2008 au 29/01/2009",
+        location: "AFPA - Liévin",
         actions: [
             {
                 id: 1,
-                description: "Montée en compétence en utilisant React JS et NodeJs"
+                description: "Contrôles surfaciques : ressuage, magnétoscopie"
             },
             {
                 id: 2,
-                description: "Réalisation d'un site web e-commerce"
+                description: "Contrôles volumiques : radiographies, ultrasons"
             },
         ]
     },
     {
-        id: 2,
-        title: "Développeur Web et Web mobile",
+        id: 5,
+        title: "Attestation de compétence informatique TIMP@SS",
+        dates: "Juin à juillet 2013",
+        location: "Instep - Liévin",
+        actions: [
+            {
+                id: 1,
+                description: "'initiation aux outils bureautiques (traitement de texte, tableur, ...)"
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Formation Développeur Web",
+        dates: "06/2017 au 12/2017",
+        location: "Popschool - Lens",
+        actions: [
+            {
+                id: 1,
+                description: "Technologies Web : HTML5/CSS3, Javascript, Bootstrap"
+            },
+            {
+                id: 2,
+                description: "Langages de programmation : PHP 7, MySQL"
+            },
+            {
+                id: 3,
+                description: "Bases de données : MySQL"
+            },
+        ]
+    },
+    {
+        id: 7,
+        title: "Formation Développeur Web et Web mobile",
         dates: "20/12/2019 au 6/10/2020",
         location: "AFPA - Liévin",
         actions: [
@@ -40,89 +108,21 @@ const FormationsInfo = [
         ]
     },
     {
-        id: 3,
-        title: "Développeur Web",
-        dates: "06/2017 au 12/2017",
-        location: "Popschool - Lens",
-        actions: [
-            {
-                id: 1,
-                description: "Technologies Web : HTML5/CSS3, Javascript, Bootstrap"
-            },
-            {
-                id: 2,
-                description: "Langages de programmation : PHP 7, MySQL"
-            },
-            {
-                id: 3,
-                description: "Bases de données : MySQL"
-            },
-        ]
-    },
-    {
-        id: 4,
-        title: "Attestation de compétence informatique TIMP@SS",
-        dates: "Juin à juillet 2013",
-        location: "Instep - Liévin",
-        actions: [
-            {
-                id: 1,
-                description: "'initiation aux outils bureautiques (traitement de texte, tableur, ...)"
-            }
-        ]
-    },
-    {
-        id: 5,
-        title: "Technicien en controle non destructif",
-        dates: "4/02/2008 au 29/01/2009",
-        location: "AFPA - Liévin",
-        actions: [
-            {
-                id: 1,
-                description: "Contrôles surfaciques : ressuage, magnétoscopie"
-            },
-            {
-                id: 2,
-                description: "Contrôles volumiques : radiographies, ultrasons"
-            },
-        ]
-    },
-    {
-        id: 6,
-        title: "Niveau BTS génie Optique",
-        dates: "09/2003 au 07/2005",
-        location: "Lycée Gustave Eiffel - Armentières",
-        // actions: [
-        //     {
-        //         id:1,
-        //         description: "Conception de produits faisant intervenir la lumière"
-        //     }
-        // ]
-    },
-    {
-        id: 7,
-        title: "1ère année BTS Physiques Industrie et Laboratoire",
-        dates: "09/2002 au 07/2003",
-        location: "Lycée Gustave Eiffel - Armentières",
-        // actions: [
-        //     {
-        //         id:1,
-        //         description: "Conception de produits faisant intervenir la lumière"
-        //     }
-        // ]
-    },
-    {
         id: 8,
-        title: "Bac Sciences de Technologies et Laboratoire",
-        dates: "09/2001 au 07/2002",
-        location: "Lycée Henri Darras - Liévin",
-        // actions: [
-        //     {
-        //         id:1,
-        //         description: "Conception de produits faisant intervenir la lumière"
-        //     }
-        // ]
-    }
+        title: "Incubateur Numérique",
+        dates: "13/10/2021 au 10/02/2022",
+        location: "1 FORMATION = 1 JOB - Lille",
+        actions: [
+            {
+                id: 1,
+                description: "Montée en compétence en utilisant React JS et NodeJs"
+            },
+            {
+                id: 2,
+                description: "Réalisation d'un site web e-commerce"
+            },
+        ]
+    },
 ]
 
 export default FormationsInfo;
--- src/pages/Formations.js
+++ src/pages/Formations.js
@@ -1,27 +1,50 @@
+import { Disclosure, Transition } from '@headlessui/react';
+import { ChevronUpIcon } from '@heroicons/react/solid'
 import React from 'react';
 import FormationsInfo from '../components/datas/formationsData';
 
 const Formations = () => {
-    const firstData = FormationsInfo.length;
+    // Tri d'apparition des projets;
+    FormationsInfo.sort(function (a, b) {
+        return b.id - a.id;
+    });
+
     return (
         <div className='flex items-center justify-center mt-20 mb-20'>
             <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-5 w-full'>
                 <h1 className='text-center text-div-blue font-roboto font-semibold'>Formations</h1>
                 {FormationsInfo.map(item =>
-                    <div key={item.id}>
-                        <div className="xl:flex xl:justify-between p-2">
-                            <div>
-                                <h3 className='text-div-red font-roboto'>{item.title}</h3>
-                                <p className='text-div-blue'>{item.dates}</p>
-                            </div>
-                            <div>
-                                <p className='text-div-green xl:text-xl'>{item.location}</p>
-                            </div>
-                        </div>
-                        <div className={`${item.id !== firstData && 'border-t-2 mx-2 my-2 border-white'}`}></div>
-                    </div>
+                    // <div key={item.id} className="flex">
 
+                    //     <div className="xl:flex xl:justify-between p-2">
+                    //         <div>
+                    //             <h3 className='text-div-red font-roboto'>{item.title}</h3>
+                    //             <p className='text-div-blue'>{item.dates}</p>
+                    //         </div>
+                    //         <div>
+                    //             <p className='text-div-green xl:text-lg'>{item.location}</p>
+                    //         </div>
+                    //     </div>
+                    //     <div className={`${item.id !== 1 && 'border-t-2 mx-2 my-2 border-white'}`}></div>
+                    // </div>
+                    <Disclosure as="div" className="mt-2" key={item.id}>
+                        {({ open }) => (
+                            <>
+                                <Disclosure.Button className="flex justify-between w-full px-4 py-2 bg-green-100 rounded-lg hover:bg-div-green focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                                    <h2 className='font-medium text-left text-div-red '>{item.title}</h2>
+                                    <ChevronUpIcon
+                                        className={`${open && 'transform rotate-180'} w-8 h-8 text-div-red`}
+                                    />
+                                </Disclosure.Button>
+                                <Disclosure.Panel className="px-4 pt-4 pb-2 flex flex-col xl:flex-row xl:justify-between">
+                                    <h3 className='text-div-blue font-semibold text-center'>{item.location}</h3>
+                                    <p className='text-div-blue font-semibold text-sm text-center'>{item.dates}</p>
+                                </Disclosure.Panel>
+                            </>
+                        )}
+                    </Disclosure>
                 )}
+
             </div>
         </div>
     );
