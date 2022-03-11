2c41112a5a07e41243f94f576cb725bf5d95ad84
Rémi Willebrouck <willremi@gmail.com>
2022-03-11 09:08:25 +0100

    début de la branche experiences

--- src/pages/Experiences.js
+++ src/pages/Experiences.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Experiences = () => {
     return (
-        <div>
+        <div className='flex items-center justify-center mt-20 mb-20'>
+            <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-5 w-full'>
+            
             page Expériences prof
+            </div>
         </div>
     );
 };
