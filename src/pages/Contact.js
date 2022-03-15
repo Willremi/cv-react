f55929f0589db0dd43b3bfad66688003ee406c82
Rémi Willebrouck <willremi@gmail.com>
2022-03-15 14:08:45 +0100

    démarrage d'une nouvelle branche

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            Me contacter
+        <div className='flex items-center justify-center mt-20 mb-20'>
+            <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-5 w-full'>
+                <h1 className='text-center text-div-blue font-roboto font-semibold'>Me contacter</h1>
+                <div className="border-t-2 my-4 border-div-green"></div>
+            </div>
         </div>
     );
 };
