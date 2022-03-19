39ec12ce79740ce0c14143c004a3e919f1ad9dd4
Rémi Willebrouck <willremi@gmail.com>
2022-03-19 20:14:44 +0100

    ajout formulaire

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,6 +6,25 @@ const Contact = () => {
             <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-5 w-full'>
                 <h1 className='text-center text-div-blue font-roboto font-semibold'>Me contacter</h1>
                 <div className="border-t-2 my-4 border-div-green"></div>
+
+                <form name="contact" method="post" data-netlify="true" onSubmit="submit">
+                    <input type="hidden" name="form-name" value="contact" />
+                    
+                        <label for="name">Votre nom:</label>
+                        <input type="text" name="name" />
+                    
+                        <label for="email">Votre email:</label>
+                        <input type="email" name="email" />
+                    
+                    
+                        <label for="message">Message:</label>
+                        <textarea name="message"></textarea>
+                    
+                    
+                        <button type="submit">Send</button>
+                    
+                </form>
+
             </div>
         </div>
     );
