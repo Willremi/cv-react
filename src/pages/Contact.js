33ff16f438abbd5157ae6955ac24d9396715a181
Rémi Willebrouck <willremi@gmail.com>
2022-03-19 20:42:18 +0100

    stylisation du formulaire

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,22 +7,24 @@ const Contact = () => {
                 <h1 className='text-center text-div-blue font-roboto font-semibold'>Me contacter</h1>
                 <div className="border-t-2 my-4 border-div-green"></div>
 
-                <form name="contact" method="post" data-netlify="true" onSubmit="submit">
+                <form name="contact" method="post" data-netlify="true" onSubmit="submit"
+                    className=' flex flex-col w-1/2 mx-auto'
+                >
                     <input type="hidden" name="form-name" value="contact" />
-                    
-                        <label for="name">Votre nom:</label>
-                        <input type="text" name="name" />
-                    
-                        <label for="email">Votre email:</label>
-                        <input type="email" name="email" />
-                    
-                    
-                        <label for="message">Message:</label>
-                        <textarea name="message"></textarea>
-                    
-                    
-                        <button type="submit">Send</button>
-                    
+
+                    <label for="name">Votre nom</label>
+                    <input type="text" name="name" />
+
+                    <label for="email" className='mt-2'>Votre email</label>
+                    <input type="email" name="email" />
+
+
+                    <label for="message" className='mt-2'>Message</label>
+                    <textarea name="message"></textarea>
+
+
+                    <button type="submit">Send</button>
+
                 </form>
 
             </div>
