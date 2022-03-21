d519030984ba03a16eda497fb6b527b64eb91957
Rémi Willebrouck <willremi@gmail.com>
2022-03-21 13:35:43 +0100

    mis en place formulaire avec formik

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,4 @@
+import { Field, Formik } from 'formik';
 import React from 'react';
 
 const Contact = () => {
@@ -7,7 +8,7 @@ const Contact = () => {
                 <h1 className='text-center text-div-blue font-roboto font-semibold'>Me contacter</h1>
                 <div className="border-t-2 my-4 border-div-green"></div>
 
-                <form name="contact" method="post" data-netlify="true" onSubmit="submit"
+                {/* <form name="contact" method="post" data-netlify="true" onSubmit="submit"
                     className='flex flex-col xl:w-1/2 xl:mx-auto'
                 >
                     <input type="hidden" name="form-name" value="contact" />
@@ -23,9 +24,51 @@ const Contact = () => {
                     <textarea name="message"></textarea>
 
 
-                    <button type="submit" className='mt-3 border-2 border-div-green bg-div-green w-2/5 xl:w-1/3 p-1 mx-auto rounded-md text-white'>Envoyer</button>
+                    <button type="submit" className='mt-3 border-2 border-div-green bg-div-green w-2/5 xl:w-1/3 p-1 mx-auto rounded-md text-white hover:bg-white hover:text-div-green font-semibold'>Envoyer</button>
 
-                </form>
+                </form> */}
+                <Formik
+                    initialValues={{ name: '', email: '', message: '' }}
+                    onSubmit={values => console.log(values)}
+                    render={({
+                        isSubmitting,
+                        handleSubmit,
+                        handleReset,
+                    }) => (
+                        <form name='contact'
+                            onSubmit={handleSubmit}
+                            onReset={handleReset}
+                            data-netlify='true'
+                            data-netlify-honeypot='bot-field'
+                            className='flex flex-col xl:w-1/2 xl:mx-auto'
+                        >
+                            <label htmlFor="name">Votre nom</label>
+                            <Field
+                                //   as="input"
+                                type="text"
+                                name="name"
+                            />
+
+                            <label htmlFor="email" className='mt-2'>Votre email</label>
+                            <Field
+                                type="email"
+                                name="email"
+                            />
+
+                            <label htmlFor="message" className='mt-2'>Votre message</label>
+                            <Field
+                                as="textarea"
+                                name="message"
+                            />
+
+                            <div className="flex">
+                                <button type="reset" className='mt-3 border-2 border-div-red bg-div-red w-2/5 xl:w-1/3 p-1 mx-auto rounded-md text-white hover:bg-white hover:text-div-red font-semibold' disabled={isSubmitting}>Annuler</button>
+                                <button type="submit" className='mt-3 border-2 border-div-green bg-div-green w-2/5 xl:w-1/3 p-1 mx-auto rounded-md text-white hover:bg-white hover:text-div-green font-semibold' disabled={isSubmitting}>Envoyer</button>
+                            </div>
+                        </form>
+                    )
+                    }
+                />
 
             </div>
         </div>
