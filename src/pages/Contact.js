c563df706183a22b225b293312041cd5f576bbd9
Rémi Willebrouck <willremi@gmail.com>
2022-03-22 20:58:29 +0100

    traitement des données du formulaire avec yup

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,7 @@ const encode = (data) => {
 
 const Contact = () => {
     return (
-        <div className='flex items-center justify-center mt-20 mb-20'>
+        <div className='flex items-center justify-center h-screen'>
             <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-5 w-full'>
                 <h1 className='text-center text-div-blue font-roboto font-semibold'>Me contacter</h1>
                 <div className="border-t-2 my-4 border-div-green"></div>
