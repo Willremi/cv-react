f64cf4851fd1b1e9da56679c8672c98271972f2b
Rémi Willebrouck <willremi@gmail.com>
2022-03-27 20:19:51 +0200

    modification de la page contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,26 +17,6 @@ const Contact = () => {
             <div className='bg-div-white rounded-lg border shadow-lg mx-5 md:mx-36 p-5 w-full'>
                 <h1 className='text-center text-div-blue font-roboto font-semibold'>Me contacter</h1>
                 <div className="border-t-2 my-4 border-div-green"></div>
-
-                {/* <form name="contact" method="post" data-netlify="true" onSubmit="submit"
-                    className='flex flex-col xl:w-1/2 xl:mx-auto'
-                >
-                    <input type="hidden" name="form-name" value="contact" />
-
-                    <label for="name">Votre nom</label>
-                    <input type="text" name="name" />
-
-                    <label for="email" className='mt-2'>Votre email</label>
-                    <input type="email" name="email" />
-
-
-                    <label for="message" className='mt-2'>Message</label>
-                    <textarea name="message"></textarea>
-
-
-                    <button type="submit" className='mt-3 border-2 border-div-green bg-div-green w-2/5 xl:w-1/3 p-1 mx-auto rounded-md text-white hover:bg-white hover:text-div-green font-semibold'>Envoyer</button>
-
-                </form> */}
                 <Formik
                     initialValues={{ name: '', email: '', message: '' }}
                     validationSchema={validationSchema}
