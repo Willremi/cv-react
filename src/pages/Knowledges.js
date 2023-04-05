4137dea86ea3f62210e4a027507f15f2b6c1ef19
Rémi Willebrouck <willremi@gmail.com>
2023-04-05 20:21:39 +0200

    correction de texte dans compétences

--- src/pages/Knowledges.js
+++ src/pages/Knowledges.js
@@ -61,7 +61,7 @@ const Knowledges = () => {
                             <div className="border-t-2 my-4 border-div-green"></div>
                             <div className="m-1">
                                 <p className='font-roboto text-red-600 indent-5'>
-                                    Je suis un développeur web qui aime la simplicité et l'élégance, j'aime créer selon les besoins des clients afin de leur proposer quelque chose au plus proche de leurs attentes. Lors du développement d'un site, je réffléchis d'un point de vue utilisateur afin de créer une interface agréable d'utilisation.
+                                    Je suis un développeur web qui aime la simplicité et l'élégance, j'aime créer selon les besoins des clients afin de leur proposer quelque chose au plus proche de leurs attentes. Lors du développement d'un site, je réfléchis d'un point de vue utilisateur afin de créer une interface agréable d'utilisation.
                                 </p>
                             </div>
                         </div>
