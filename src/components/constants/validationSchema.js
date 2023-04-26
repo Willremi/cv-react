309d5dec11f230d2c70fac7abcd26229581e97dd
Rémi Willebrouck <willremi@gmail.com>
2023-04-26 15:47:55 +0200

    ajout emailjs pour formulaire de contact

--- src/components/constants/validationSchema.js
+++ src/components/constants/validationSchema.js
@@ -9,6 +9,10 @@ const validationSchema = Yup.object().shape({
   email: Yup.string()
     .matches(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, "veuillez entrez un email valide")
     .required("Votre Email est obligatoire!"),
+  subject: Yup.string()
+  .min(2, 'nom trop court!')
+  .max(50, 'nom trop long!')
+  .matches(/^[\p{L}\s]{2,}$/u, "Le nom n'est pas au bon format"),
   message: Yup.string()
     .required('Votre message est obligatoire!'),
 })
