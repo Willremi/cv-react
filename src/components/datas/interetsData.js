318d3fe4bb49f7cca0d34ff5bff002f4372e4954
Rémi Willebrouck <willremi@gmail.com>
2023-05-11 20:17:57 +0200

    ajout data des intérêts

---
+++ src/components/datas/interetsData.js
@@ -0,0 +1,24 @@
+const interetsInfo = [
+    {
+        id: 1,
+        name: "Rétrogaming",
+        icon: require('../../assets/images/icons/gaming.png')
+    },
+    {
+        id: 2,
+        name: "Histoire",
+        icon: require('../../assets/images/icons/history.png')
+    },
+    {
+        id: 3,
+        name: "Les Bandes dessinées",
+        icon: require('../../assets/images/icons/comic.png')
+    },
+    {
+        id: 4,
+        name: "Marche à pied",
+        icon: require('../../assets/images/icons/walking.png')
+    },
+];
+
+export default interetsInfo;
