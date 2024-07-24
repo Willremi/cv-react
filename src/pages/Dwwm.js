a0284fef9af585171f74604308e31290589ff919
Willremi <willremi@gmail.com>
2024-07-24 08:47:46 +0200

    update dwwm

--- src/pages/Dwwm.js
+++ src/pages/Dwwm.js
@@ -53,15 +53,15 @@ const Dwwm = () => {
                         <h2 className='text-center text-div-blue font-roboto font-semibold'>Systèmes d'exploitation</h2>
                         <div className="border-t-2 my-4 border-div-green"></div>
                         <div className="flex justify-around">
-                            <div className="w-20 text-center mx-3 xl:mx-0">
+                            <div className="w-20 lg:w-16 text-center mx-3 xl:mx-0">
                                 <img src={windows} alt="Logo de Windows" />
                                 <span className='font-roboto text-div-red'>Windows</span>
                             </div>
-                            <div className="w-20 text-center mx-3 xl:mx-0">
+                            <div className="w-20 lg:w-16 text-center mx-3 xl:mx-0">
                                 <img src={mac} alt="Logo de Mac OS" />
                                 <span className='font-roboto text-div-red'>Mac OS</span>
                             </div>
-                            <div className="w-20 text-center mx-3 xl:mx-0">
+                            <div className="w-20 lg:w-16 text-center mx-3 xl:mx-0">
                                 <img src={debian} alt="Logo de Debian" />
                                 <span className='font-roboto text-div-red'>Debian</span>
                             </div>
@@ -87,109 +87,91 @@ const Dwwm = () => {
                     </div>
                 </div>
 
+                
                 {/* Langage web & projets */}
-                <div className="md:flex mx-36 max-w-screen-3xl">
-                    <div className="flex-1 min-w-0 mx-2">
-                        <div className="bg-div-white rounded-lg border shadow-lg p-2 mt-4 w-full">
-                            <h2 className='text-center text-div-blue font-roboto font-semibold'>Langages</h2>
-                            <div className="border-t-2 my-4 border-div-green"></div>
-                            <div className='flex flex-wrap justify-center'>
-                                {DwwmInfo.map(item =>
-                                    <div className={`w-16 md:w-20 text-center mb-4`} key={item.id}>
-                                        <a href={item.href} target='_blank' rel="noreferrer">
-                                            <StyledTooltip title={item.name} arrow={true}>
-                                                <img src={item.image} alt={item.name} className="w-16" />
-                                            </StyledTooltip>
-                                            <span className='lg:hidden font-roboto text-div-red text-xs text-center'>{item.name}</span>
-                                        </a>
-                                    </div>
-                                )}
-                            </div>
+    <div className="flex flex-wrap -mx-2">
+        {/* Langages (6 colonnes sur 12) */}
+        <div className="w-full md:w-1/2 px-2">
+            <div className="bg-div-white rounded-lg border shadow-lg p-2 mt-4">
+                <h2 className='text-center text-div-blue font-roboto font-semibold'>Langages</h2>
+                <div className="border-t-2 my-4 border-div-green"></div>
+                <div className='flex flex-wrap justify-center'>
+                    {DwwmInfo.map(item => (
+                        <div className="w-16 md:w-20 text-center mb-4" key={item.id}>
+                            <a href={item.href} target='_blank' rel="noreferrer">
+                                <StyledTooltip title={item.name} arrow={true}>
+                                    <img src={item.image} alt={item.name} className="w-16" />
+                                </StyledTooltip>
+                                <span className='lg:hidden font-roboto text-div-red text-xs text-center'>{item.name}</span>
+                            </a>
                         </div>
-                    </div>
-                    <div className="flex-1 min-w-0 mx-2">
-                        <div className="bg-div-white rounded-lg border shadow-lg p-4 mt-4 w-full">
-                            <h2 className='text-center text-div-blue font-roboto font-semibold'>Projets</h2>
-                            <div className="border-t-2 my-4 border-div-green"></div>
-                            <div className='flex flex-wrap justify-around'>
-                                {Projets.map(
-                                    item =>
-                                        <div className='text-center' key={item.id}>
-                                            <h3 className='text-div-blue underline py-1 font-semibold text-lg'>{item.title}</h3>
-                                            <img src={item.images[0].src} alt={item.title} className="w-40 sm:w-56 md:w-40 mx-auto cursor-pointer" id={item.title} onClick={() => {
-                                                setShowModal(true);
-                                                setModalData(item);
-                                            }} />
-                                            <span className='text-div-red text-sm'>{item.dates}</span>
-
-                                            {showModal && (
-                                                <>
-                                                    <div
-                                                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-                                                    // onClick={() => setShowModal(false)}
-                                                    >
-                                                        <div className="relative w-auto my-6 mx-2 max-w-3xl">
-                                                            {/*content*/}
-                                                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                                                {/* header */}
-                                                                <div className="flex items-start justify-between p-4 border-b border-solid border-div-green mx-4 rounded-t">
-                                                                    <h2 className="font-semibold text-div-blue font-roboto">
-                                                                        {modalData.title}
-                                                                    </h2>
-                                                                </div>
-                                                                {/*body*/}
-                                                                <div className="relative px-6 p-4 flex-auto">
-                                                                    <p className="my-2 text-red-800 text-xl leading-relaxed font-roboto">
-                                                                        {modalData.description}
-                                                                    </p>
-                                                                    <div className="flex justify-center">
-                                                                        {modalData.technos.map(elt =>
-
-                                                                            <div key={elt.id}
-                                                                                className="mx-2 px-2"
-                                                                            >
-                                                                                <StyledTooltip title={elt.title} arrow={true}>
-                                                                                    <img src={elt.logo} alt={elt.title}
-                                                                                        className="w-10 xl:w-12 h-auto"
-                                                                                    />
-                                                                                </StyledTooltip>
-                                                                            </div>
-                                                                        )}
-                                                                    </div>
-                                                                    <div className="overflow-auto max-h-44 mx-10 mt-2">
-                                                                        {/* <Carousel useKeyboardArrows={true} showThumbs={false} autoPlay={true} infiniteLoop={true} interval={5000} showStatus={false}>
-                                                                                {modalData.images.slice(1).map(elt =>
-                                                                                    <div key={elt.id} className='slide'>
-                                                                                        {elt.legend && <h6 className='mb-2 text-div-blue font-semibold'>"{elt.legend}"</h6>}
-                                                                                        <img src={elt.src} alt={elt.id} />
-                                                                                    </div>
-                                                                                )}
-                                                                            </Carousel> */}
-                                                                        <img src={modalData.images[1].src} alt={modalData.title} />
-                                                                    </div>
-                                                                </div>
-                                                                {/*footer*/}
-                                                                <div className={`flex items-center p-4 mx-4 border-t border-solid border-div-green rounded-b ${(!modalData.lien) ? 'justify-end' : 'justify-between'}`}>
-                                                                    {modalData.lien && <a href={modalData.lien} rel="noreferrer"><button className='uppercase text-div-blue font-bold text-sm'>Visiter le site</button></a>}
-                                                                    <button
-                                                                        className="text-red-500 background-transparent font-bold uppercase text-sm outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-                                                                        type="button"
-                                                                        onClick={() => setShowModal(false)}
-                                                                    >
-                                                                        Fermer la fenêtre
-                                                                    </button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </>
-                                            )}
-                                        </div>
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
+            </div>
+        </div>
+        {/* Projets (6 colonnes sur 12) */}
+        <div className="w-full md:w-1/2 px-2">
+            <div className="bg-div-white rounded-lg border shadow-lg mt-4 p-2">
+                <h2 className='text-center text-div-blue font-roboto font-semibold'>Projets</h2>
+                <div className="border-t-2 my-4 border-div-green"></div>
+                <div className='flex flex-wrap justify-around'>
+                    {Projets.map(item => (
+                        <div className='text-center' key={item.id}>
+                            <h3 className='text-div-blue underline py-1 font-semibold text-lg'>{item.title}</h3>
+                            <img src={item.images[0].src} alt={item.title} className="w-40 sm:w-56 md:w-40 mx-auto cursor-pointer" id={item.title} onClick={() => {
+                                setShowModal(true);
+                                setModalData(item);
+                            }} />
+                            <span className='text-div-red text-sm'>{item.dates}</span>
+                            {showModal && (
+                                <>
+                                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                                        <div className="relative w-auto my-6 mx-2 max-w-3xl">
+                                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                                <div className="flex items-start justify-between p-4 border-b border-solid border-div-green mx-4 rounded-t">
+                                                    <h2 className="font-semibold text-div-blue font-roboto">
+                                                        {modalData.title}
+                                                    </h2>
+                                                </div>
+                                                <div className="relative px-6 p-4 flex-auto">
+                                                    <p className="my-2 text-red-800 text-xl leading-relaxed font-roboto">
+                                                        {modalData.description}
+                                                    </p>
+                                                    <div className="flex justify-center">
+                                                        {modalData.technos.map(elt => (
+                                                            <div key={elt.id} className="mx-2 px-2">
+                                                                <StyledTooltip title={elt.title} arrow={true}>
+                                                                    <img src={elt.logo} alt={elt.title} className="w-10 xl:w-12 h-auto" />
+                                                                </StyledTooltip>
+                                                            </div>
+                                                        ))}
+                                                    </div>
+                                                    <div className="overflow-auto max-h-44 mx-10 mt-2">
+                                                        <img src={modalData.images[1].src} alt={modalData.title} />
+                                                    </div>
+                                                </div>
+                                                <div className={`flex items-center p-4 mx-4 border-t border-solid border-div-green rounded-b ${(!modalData.lien) ? 'justify-end' : 'justify-between'}`}>
+                                                    {modalData.lien && <a href={modalData.lien} rel="noreferrer"><button className='uppercase text-div-blue font-bold text-sm'>Visiter le site</button></a>}
+                                                    <button
+                                                        className="text-red-500 background-transparent font-bold uppercase text-sm outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                                                        type="button"
+                                                        onClick={() => setShowModal(false)}
+                                                    >
+                                                        Fermer la fenêtre
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 
             </div>
