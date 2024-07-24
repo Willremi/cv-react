74b6a72044e96084a27eae7d73e641fe04c8faa4
Rémi Willebrouck <willremi@gmail.com>
2024-07-24 19:57:55 +0200

    update tssr

--- src/pages/Tssr.js
+++ src/pages/Tssr.js
@@ -3,6 +3,7 @@ import windows from '../assets/images/logos/windows.png';
 import ws from '../assets/images/logos/ws2.png';
 import mac from '../assets/images/logos/macos.png';
 import debian from '../assets/images/logos/debian.png';
+import android from '../assets/images/logos/android.png';
 import esxi from '../assets/images/logos/esxi.png';
 import pfsense from '../assets/images/logos/pfsense.png';
 
@@ -28,39 +29,62 @@ const Tssr = () => {
                     <h2 className='text-center text-div-blue font-roboto font-semibold'>Systèmes d'exploitation</h2>
                     <div className="border-t-2 my-4 border-div-green"></div>
                     <div className="flex justify-around">
-                                <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
-                                    <img src={windows} alt="Logo de Windows" />
-                                    <span className='font-roboto text-div-red'>Windows</span>
-                                </div>
-                                <div className="w-20 md:w-14 text-center mx-3 xl:mx-0">
-                                    <img src={ws} alt="Logo de Windows Server" />
-                                    <span className='font-roboto text-div-red'>Windows Server</span>
-                                </div>
-                                <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
-                                    <img src={mac} alt="Logo de MacOS" />
-                                    <span className='font-roboto text-div-red'>Mac OS</span>
-                                </div>
-                                <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
-                                    <img src={debian} alt="Logo de Debian" />
-                                    <span className='font-roboto text-div-red'>Debian</span>
-                                </div>
-                                <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
-                                    <img src={esxi} alt="Logo d'ESXi" />
-                                    <span className='font-roboto text-div-red'>ESXi</span>
-                                </div>
-                                <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
-                                    <img src={pfsense} alt="Logo de pfSense" />
-                                    <span className='font-roboto text-div-red'>pfSense</span>
-                                </div>
-                            </div>
+                        <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
+                            <img src={windows} alt="Logo de Windows" />
+                            <span className='font-roboto text-div-red'>Windows</span>
+                        </div>
+                        <div className="w-20 md:w-14 text-center mx-3 xl:mx-0">
+                            <img src={ws} alt="Logo de Windows Server" />
+                            <span className='font-roboto text-div-red'>Windows Server</span>
+                        </div>
+                        <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
+                            <img src={mac} alt="Logo de MacOS" />
+                            <span className='font-roboto text-div-red'>Mac OS</span>
+                        </div>
+                        <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
+                            <img src={debian} alt="Logo de Debian" />
+                            <span className='font-roboto text-div-red'>Debian</span>
+                        </div>
+                        <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
+                            <img src={android} alt="Logo d'Android" />
+                            <span className='font-roboto text-div-red'>Android</span>
+                        </div>
+                        <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
+                            <img src={esxi} alt="Logo d'ESXi" />
+                            <span className='font-roboto text-div-red'>ESXi</span>
+                        </div>
+                        <div className="w-20 md:w-14 xl:w-16 text-center mx-3 xl:mx-0">
+                            <img src={pfsense} alt="Logo de pfSense" />
+                            <span className='font-roboto text-div-red'>pfSense</span>
+                        </div>
+                    </div>
                 </div>
 
                 {/* Systèmes exploitation & outils */}
-                <div className="flex space-x-4 mt-4">
-                    <div className="w-1/2 bg-gray-200 p-4"></div>
-                    <div className="w-1/2 bg-gray-300 p-4"></div>
+                <div className="flex flex-wrap -mx-2">
+                    <div className="w-full md:w-1/2 px-2">
+                        <div className="bg-div-white rounded-lg border shadow-lg p-2 mt-4">
+                            <h2 className='text-center text-div-blue font-roboto font-semibold'>Technologies informatiques</h2>
+                            <div className="border-t-2 my-4 border-div-green"></div>
+                            <div className="flex flex-wrap justify-center">
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>DNS</div>
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>DHCP</div>
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>VPN</div>
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>HTTP(S)</div>
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>SMTP</div>
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>TCP/IP</div>
+                                <div className='border rounded-lg bg-div-green p-2 text-white text-center w-20 m-1'>ICMP</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-full md:w-1/2 px-2">
+                        <div className="bg-div-white rounded-lg border shadow-lg p-2 mt-4">
+                        <h2 className='text-center text-div-blue font-roboto font-semibold'>Technologies informatiques</h2>
+                        <div className="border-t-2 my-4 border-div-green"></div>
+                        </div>
+                    </div>
                 </div>
-                
+
             </div>
         </div>
     );
